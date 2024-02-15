0a435eef5b239376f7dcc2ce857abb5e918ace91
וייס לאה < @mbjcomp.local>
2024-02-15 15:10:23 +0200

    start build tables

---
+++ Query/table.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [dbo].[Cities] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (100)            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC));
+
+CREATE TABLE [dbo].[Categories] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (100)            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC));
+
+    CREATE TABLE [dbo].[Business] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (100)            NOT NULL,
+    [Category] INT  NOT NULL,
+    [IdCities] INT NOT NULL,
+    [Adress] NVARCHAR (100) NULL,
+    [PhoneNum] NVARCHAR (10) NOT NULL,
+    [OpeningHours]  NVARCHAR (100) NULL,
+    [MoreInfo] NVARCHAR (300) NULL,
+     PRIMARY KEY CLUSTERED ([Id] ASC))
+     FOREIGN KEY (IdCities) REFERENCES Cities(Id);
+
+CREATE TABLE [dbo].[HousingUnits] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Cities] INT NOT NULL,
+    [PhoneNum] NVARCHAR (10) NOT NULL,
+    [Adress] NVARCHAR (100) NOT NULL,
+    [Mr] INT NOT NULL,
+    [Name]  NVARCHAR (100)            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC));
+    
---
+++ Query/table2.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[Cities] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (100)            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC));
+
+CREATE TABLE [dbo].[Categories] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (100)            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC));
+
+    CREATE TABLE [dbo].[Business] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (100)            NOT NULL,
+    [Category] INT  NOT NULL,
+    [CityId] INT NOT NULL,
+    [Adress] NVARCHAR (100) NULL,
+    [PhoneNum] NVARCHAR (10) NOT NULL,
+    [OpeningHours]  NVARCHAR (100) NULL,
+    [MoreInfo] NVARCHAR (300) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)),
+    FCONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID)
+    REFERENCES Cities(Id);
